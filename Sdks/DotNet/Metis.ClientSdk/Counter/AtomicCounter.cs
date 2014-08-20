0e360244409d9daaaafa7b455691633fe7de85a9
wuhy <wuhy@101.com>
2014-08-20 09:01:41 +0800

    调整项目的文件结构

---
+++ Sdks/DotNet/Metis.ClientSdk/Counter/AtomicCounter.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Metis.ClientSdk.Counter
+{
+    internal class AtomicCounter
+    {
+        private static AtomicCounter instance;
+
+        private ConcurrentDictionary<String, Counter32> mapCounter32;
+        private ConcurrentDictionary<String, Counter64> mapCounter64;
+
+        public static AtomicCounter Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new AtomicCounter();
+                }
+                return instance;
+            }
+        }
+
+        private AtomicCounter() 
+        {
+            //获取当前环境中处理器的数量
+            int numProcs = Environment.ProcessorCount;
+            //按照处理器数量的2倍, 预估对列表的同时操作线程数量
+            mapCounter32 = new ConcurrentDictionary<string, Counter32>(numProcs * 2, 100);
+            mapCounter64 = new ConcurrentDictionary<string, Counter64>(numProcs * 2, 100);
+        }
+        public int CounterCount
+        {
+            get { return mapCounter32.Count + mapCounter64.Count; }
+        }
+        public void Increase32(string key)
+        {
+            var counter = mapCounter32.GetOrAdd(key, (k) => new Counter32(k));
+            counter.Increment();
+        }
+        public void Decrease32(string key)
+        {
+            var counter = mapCounter32.GetOrAdd(key, (k) => new Counter32(k));
+            counter.Decrement();
+        }
+        public void Increase32(string key, Int32 num)
+        {
+            var counter = mapCounter32.GetOrAdd(key, (k) => new Counter32(k));
+            counter.Increment(num);
+        }
+        public void Decrease32(string key, Int32 num)
+        {
+            var counter = mapCounter32.GetOrAdd(key, (k) => new Counter32(k));
+            counter.Decrement(num);
+        }
+        public void Increase64(string key)
+        {
+            var counter = mapCounter64.GetOrAdd(key, (k) => new Counter64(k));
+            counter.Increment();
+        }
+        public void Decrease64(string key)
+        {
+            var counter = mapCounter64.GetOrAdd(key, (k) => new Counter64(k));
+            counter.Decrement();
+        }
+        public void Increase64(string key, Int64 num)
+        {
+            var counter = mapCounter64.GetOrAdd(key, (k) => new Counter64(k));
+            counter.Increment(num);
+        }
+        public void Decrease64(string key, Int64 num)
+        {
+            var counter = mapCounter64.GetOrAdd(key, (k) => new Counter64(k));
+            counter.Decrement(num);
+        }
+        public Int32 Reset32(string key)
+        {
+            var counter = mapCounter32.GetOrAdd(key, (k) => new Counter32(k));
+            return counter.Reset();
+        }
+        public Int64 Reset64(string key)
+        {
+            var counter = mapCounter64.GetOrAdd(key, (k) => new Counter64(k));
+            return counter.Reset();
+        }
+        public Counter32 Get32(string key)
+        {
+            Counter32 counter;
+            if (!mapCounter32.TryGetValue(key, out counter))
+            {
+                return null;
+            }
+            return counter;
+        }
+        public Counter64 Get64(string key)
+        {
+            Counter64 counter;
+            if (!mapCounter64.TryGetValue(key, out counter))
+            {
+                return null;
+            }
+            return counter;
+        }
+        public IList<KeyValuePair<string, Int64>> GetAll()
+        {
+            IList<KeyValuePair<string, Int64>> datas = new List<KeyValuePair<string, Int64>>();
+            datas.AddRange(mapCounter32.ToArray().Select(o => new KeyValuePair<string, Int64>(o.Key, o.Value.Counter)));
+            datas.AddRange(mapCounter64.ToArray().Select(o=>new KeyValuePair<string,Int64>(o.Key, o.Value.Counter)));
+            return datas;
+        }
+        public void Clear()
+        {
+            mapCounter32.Clear();
+            mapCounter64.Clear();
+        }
+    }
+}
---
+++ Sdks/DotNet/Metis.ClientSdk/Gatherer/BaseGatherer.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace Metis.ClientSdk.Gatherer
+{
+    /// <summary>
+    /// 采集器基础上下文对象
+    /// </summary>
+    internal abstract class BaseGatherer
+    {
+        //当前IIS的部署环境
+        private static WebServerType iisVersion = WebServerType.Unknown;
+        private bool debugRequest = false;
+        //当前请求的上下文
+        protected HttpApplication application;
+
+        static BaseGatherer()
+        {
+            iisVersion = WebServer.GetIISServerType(); 
+        }
+        /// <summary>
+        /// 当前IIS的部署环境
+        /// </summary>
+        protected static WebServerType IISVersion { get { return iisVersion; } }
+        /// <summary>
+        /// 采集者名称
+        /// </summary>
+        public abstract string Name { get; }
+        /// <summary>
+        /// 采集者描述
+        /// </summary>
+        public abstract string Description { get; }
+        /// <summary>
+        /// 是否为Debug的请求
+        /// </summary>
+        protected bool DebugRequest { get { return debugRequest; } }
+        public virtual void BeginRequest() { }
+        public virtual void EndRequest() { }
+        public virtual void ExceptionOccur() { }
+        public abstract void Dispose();
+        /// <summary>
+        /// 获取URL中?之前的字符串
+        /// </summary>
+        /// <param name="url">原始的URL字符串</param>
+        /// <returns>处理后的字符串</returns>
+        protected string GetPureUrl(string url)
+        {
+            int count = url.IndexOf('?');
+            if (count == -1)
+                return url;
+            return url.Substring(0, count);
+        }
+        /// <summary>
+        /// 处理Debug页面的请求
+        /// </summary>
+        protected void HandleDebugInfo()
+        {
+            if (application.Request.RawUrl.Equals("/gatherer/debug"))
+            {
+                debugRequest = true;
+                if (!GathererContext.Current.IsDebug)
+                {
+                    application.Response.Write("Gatherer DEBUG mode is off");
+                    application.CompleteRequest();
+                }
+                else
+                {
+                    string logInfos = GathererLogger.Instance.Read();
+                    application.Response.Write(logInfos);
+                    application.CompleteRequest();
+                }
+            }
+        }
+
+
+
+
+    }
+}
