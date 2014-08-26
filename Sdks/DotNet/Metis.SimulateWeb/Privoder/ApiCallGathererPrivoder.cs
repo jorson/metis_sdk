0edda5cc1a76916094da5ac98c65d9b5b211f6a8
wuhy <wuhy@101.com>
2014-08-26 20:07:53 +0800

    添加了模拟站点的代码

--- Sdks/DotNet/Metis.SimulateWeb/Privoder/ApiCallGathererPrivoder.cs
+++ Sdks/DotNet/Metis.SimulateWeb/Privoder/ApiCallGathererPrivoder.cs
@@ -7,9 +7,28 @@ namespace Metis.SimulateWeb
 {
     public class ApiCallGathererPrivoder : Metis.ClientSdk.IGathererDataPrivoder
     {
+        Nd.OAuthClient.AccessGrant accessGrant = null;
+        string clientId = System.Configuration.ConfigurationManager.AppSettings["oauth.client.id"];
+        string clientSecret = System.Configuration.ConfigurationManager.AppSettings["oauth.client.secret"];
+
         public string GetAccesstoken()
         {
-            return "69d63fadaee842a4a5cc575f8eeff707";
+            if (accessGrant == null || accessGrant.IsExpire())
+            {   
+                int id = 70;
+                Int32.TryParse(clientId, out id);
+                Nd.OAuthClient.TokenData data = new Nd.OAuthClient.ClientCredentialsTokenData(id, clientSecret);
+                var result = Nd.OAuthClient.OAuthService.Authorize(data);
+                if (result.Data != null)
+                {
+                    return result.Data.AccessToken;
+                }
+                else
+                {
+                    return "69d63fadaee842a4a5cc575f8eeff707";
+                }
+            }
+            return accessGrant.AccessToken;
         }
 
         public IDictionary<string, object> GetExtendData(HttpContext context)
